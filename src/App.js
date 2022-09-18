6d27a3a123ce832dc28d581802a4d51f2388dc2f
manish <Manishbisht9711@gmail.com>
2022-09-18 19:30:22 +0530

    progress in next page

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
 import "./App.css";
-import Slider from "./components/LandingPageslider/slider";
+import Home from "./pages/home";
 
 function App() {
   return (
     <div className="App">
-      <Slider />
-
-      <section>
-        <h4>hello</h4>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem
-          dolore doloribus debitis eaque ut nisi, ex, assumenda possimus, aut
-          molestiae natus laborum enim molestias? Voluptatum quo eos veritatis
-          minima, unde, modi delectus vitae deleniti dolor architecto veniam
-          culpa deserunt? Vel adipisci totam quam voluptas eius quidem id
-          accusantium tempore sed molestias, alias iusto distinctio, impedit
-          odit soluta minima commodi ipsam cum veritatis. Eveniet, error
-          voluptatum dolore maiores incidunt ipsam at vel tempora quos voluptas
-          dolorem placeat enim magnam voluptate labore reprehenderit deleniti
-          sed ipsum ullam consequatur? Illo, sed quibusdam, laborum nobis cumque
-          aperiam fugit, corporis magnam fugiat rerum numquam possimus!
-        </p>
-      </section>
+      <Home />
     </div>
   );
 }
---
+++ src/pages/home.js
@@ -0,0 +1,155 @@
+import React, { useEffect, useRef } from "react";
+import Slider from "../components/LandingPageslider/slider";
+import "./home.css";
+import image from "../assets/backgrounds/bg2.jpg";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const Home = () => {
+  const bgref = useRef(null);
+  const birdbgref = useRef(null);
+
+  useEffect(() => {
+    function pinBackground() {
+      const t1 = gsap
+        .timeline({
+          delay: 1,
+          scrollTrigger: {
+            trigger: bgref.current,
+            start: "top top",
+            end: "+=100%",
+            pin: true,
+            scrub: true,
+          },
+        })
+
+        .to("#wildheading", {
+          y: -400,
+          opacity: 1,
+          scale: 1,
+          duration: 2,
+          delay: function (index) {
+            return 2 * index;
+          },
+        })
+        .to(bgref.current, { scale: 1 }, "+=0.05");
+
+      return t1;
+    }
+
+    const mastertimeline = gsap.timeline();
+    mastertimeline.add(pinBackground());
+  }, []);
+
+  return (
+    <div className="home">
+      <div className="HomePageSlider">
+        <Slider />
+      </div>
+      <div className="wildLifeStats">
+        <div ref={bgref} className="WildLifeHeadingbg"></div>
+        <div className="WildLifeHeading">
+          <h2 id="wildheading">
+            <span>Over</span> 1.7 million <span>Species</span>
+          </h2>
+          <h2 id="wildheading" className="Letsview">
+            Lets View Few Of Them
+          </h2>
+        </div>
+        <section className="statbox">
+          <div className="BirdStats">
+            <div className="birdwrapper">
+              <h2>Birds</h2>
+              <h4>Total Species</h4>
+              <p>
+                We see that there are more than 11,000 described bird species in
+                the world.
+              </p>
+              <button>View Article</button>
+            </div>
+            <div className="birdwrapper">
+              <h2>Birds</h2>
+              <h4>Total Species</h4>
+              <p>
+                We see that there are more than 11,000 described bird species in
+                the world.
+              </p>
+              <button>View Article</button>
+            </div>
+            <div className="birdwrapper">
+              <h2>Birds</h2>
+              <h4>Total Species</h4>
+              <p>
+                We see that there are more than 11,000 described bird species in
+                the world.
+              </p>
+              <button>View Article</button>
+            </div>
+          </div>
+          <div className="BirdStats">
+            <div className="birdwrapper">
+              <h2>Animals</h2>
+              <h4>Total Species</h4>
+              <p>
+                We see that there are more than 11,000 described bird species in
+                the world.
+              </p>
+              <button>View Article</button>
+            </div>
+            <div className="birdwrapper">
+              <h2>Animals</h2>
+              <h4>Total Species</h4>
+              <p>
+                We see that there are more than 11,000 described bird species in
+                the world.
+              </p>
+              <button>View Article</button>
+            </div>
+            <div className="birdwrapper">
+              <h2>Animals</h2>
+              <h4>Total Species</h4>
+              <p>
+                We see that there are more than 11,000 described bird species in
+                the world.
+              </p>
+              <button>View Article</button>
+            </div>
+          </div>
+          <div className="BirdStats">
+            <div className="birdwrapper">
+              <h2>Swimmer</h2>
+              <h4>Total Species</h4>
+              <p>
+                We see that there are more than 11,000 described bird species in
+                the world.
+              </p>
+              <button>View Article</button>
+            </div>
+            <div className="birdwrapper">
+              <h2>Swimmer</h2>
+              <h4>Total Species</h4>
+              <p>
+                We see that there are more than 11,000 described bird species in
+                the world.
+              </p>
+              <button>View Article</button>
+            </div>
+            <div className="birdwrapper">
+              <h2>Swimmer</h2>
+              <h4>Total Species</h4>
+              <p>
+                We see that there are more than 11,000 described bird species in
+                the world.
+              </p>
+              <button>View Article</button>
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
